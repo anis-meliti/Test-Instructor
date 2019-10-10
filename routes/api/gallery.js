a73ba517b9c809bd26c3b7f3cc90bdbabcb89af3
anisMelliti <ansmeliti@gmail.com>
2019-10-10 16:18:42 +0100

    implement the back end put and get pic

---
+++ routes/api/gallery.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const Gallery = require('../../models/Gallery');
+
+// @route   POST api/user
+// @desc    Register route
+// @access  Public
+router.post(
+  '/:id',
+  [
+    check('title', 'title is required')
+      .not()
+      .isEmpty(),
+    check('path', 'path is required')
+      .not()
+      .isEmpty()
+  ],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+    const galleryFields = {};
+    const { title, path } = req.body;
+    galleryFields.user = req.params.id;
+    if (title) galleryFields.title = title;
+    if (path) galleryFields.path = path;
+    try {
+      gallery = new Gallery(galleryFields);
+      await gallery.save();
+      res.json(gallery);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
+  }
+);
+// @route   GET api/Gallery
+// @desc    GET route
+// @access  Public
+
+router.get('/:id', async (req, res) => {
+  try {
+    const gallery = await User.find({ user: req.params.id }).populate(
+      'gallery',
+      ['title', 'path']
+    );
+    !user ? res.status(400).json({ msg: 'server error' }) : res.json(gallery);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('server error');
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json({ extended: false }));
 // app.get('/', (req, res) => res.send(res.json));
 // Define Routes
 app.use('/users', require('./routes/api/User'));
+app.use('/gallery', require('./routes/api/gallery'));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, err =>
